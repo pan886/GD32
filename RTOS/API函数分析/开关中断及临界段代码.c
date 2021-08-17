b0ca72ffeb08e918ac028aac6f97ea2d49d9f329
pan886 <1348987953@qq.com>
2021-08-17 11:48:59 +0800

    some comment

--- RTOS/API函数分析/开关中断及临界段代码.c
+++ RTOS/API函数分析/开关中断及临界段代码.c
@@ -1,3 +1,5 @@
+/*author:panpan created at 20210816*/
+
 开关中断函数为portENABLE_INTERRUPTS() 和portDISABLE_INTERRUPTS()
 #define portDISABLE_INTERRUPTS()                vPortRaiseBASEPRI()
 #define portENABLE_INTERRUPTS()                 vPortSetBASEPRI(0)
@@ -15,20 +17,21 @@ __STATIC_FORCEINLINE void __ECLIC_SetMth(uint8_t mth)
 {
     ECLIC->MTH = mth;
 }
-
-
-可以看到，这个函数向mth寄存器写入值。
+/*可以看到，这个函数向mth寄存器写入值。
 mth 寄存器是完整的 8 位寄存器，所有位均可读可写，软件可以通过写此寄存器配置目
 标阈值。注意：该阈值表征的是一种级别（Level）的数字值。
 ECLIC 最终仲裁出的中断的“级别（Level）数字值”只有高于“mth 寄存器中的值”，
 该中断才能够被发送给处理器内核。
 
 所以，如果要打开所有中断，那么将mth的阈值设为0即可。
-如果要关闭中断，那么将mth的阈值设为255即可。
+如果要关闭中断，那么将mth的阈值设为255即可。*/
+/*
+taskENTER_CRITICAL() 进入临界段
+taskENTER_CRITICAL_FROM_ISR()
+taskEXIT_CRITICAL()  退出临界段
+taskEXIT_CRITICAL_FROM_ISR( x )
 
-
-临界段函数
-
+*/
 void vPortEnterCritical( void )//进入临界段
 {
     portDISABLE_INTERRUPTS(); //关中断
@@ -45,7 +48,9 @@ void vPortEnterCritical( void )//进入临界段
     }
 }
 
-
+/*注意：uxCriticalNesting这个值static UBaseType_t uxCriticalNesting = 0xaaaaaaaa;
+这个值在xPortStartScheduler函数中置0
+*/
 
 void vPortExitCritical( void )//退出临界段
 {
@@ -57,9 +62,30 @@ void vPortExitCritical( void )//退出临界段
     }
 }
 
-注意：uxCriticalNesting这个值static UBaseType_t uxCriticalNesting = 0xaaaaaaaa;
-这个值在xPortStartScheduler函数中置0
 
+/*
+中断函数临界段代码最终调用以下函数
+*/
+portFORCE_INLINE static uint8_t ulPortRaiseBASEPRI( void )
+{
+    uint8_t ulOriginalBASEPRI;
+
+    ulOriginalBASEPRI = ECLIC_GetMth();
+    ECLIC_SetMth(uxMaxSysCallMTH);//设置阈值为255
+    __RWMB();// __FENCE(iorw,iorw),Fence on all memory and I/O
+
+    /* This return might not be reached but is necessary to prevent compiler
+    warnings. */
+    return ulOriginalBASEPRI;
+}
+
+
+
+portFORCE_INLINE static void vPortSetBASEPRI( uint8_t ulNewMaskValue )
+{
+    ECLIC_SetMth(ulNewMaskValue);
+    __RWMB();
+}
 
 
 
