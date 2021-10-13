4e5ca06d5c61890e9f557aa86ba63c3d33b2796e
pan886 <1348987953@qq.com>
2021-10-13 11:15:47 +0800

    noise

---
+++ DAC/双路噪声波/main.c
@@ -0,0 +1,133 @@
+
+#include "gd32vf103.h"
+#include "gd32vf103_timer.h"
+#include "gd32vf103_gpio.h"
+#include "gd32vf103_usart.h"
+#include "n200_func.h"
+#include <stdio.h>
+#include "gd32vf103_dac.h"
+#include "gd32vf103_i2c.h"
+#include "gd32vf103v_rvstar.h"
+#include "mpu.h"
+#include "i2c.h"
+#include "gd32vf103_eclic.h"
+#include "gd32vf103_bkp.h"
+#include "gd32vf103_dma.h"
+
+#include "gd32vf103.h"
+
+#include "nuclei_sdk_soc.h"
+#include "gd32vf103v_rvstar.h"
+#include "gd32vf103.h"
+#include "gd32vf103_exmc.h"
+#include "exmc_sram.h"
+
+void rcu_config(void);
+void gpio_config(void);
+void dac_config(void);
+void timer5_config(void);
+void timer6_config(void);
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    rcu_config();
+    gpio_config();
+    dac_config();
+    timer5_config();
+    timer6_config();
+    while (1){
+    }
+}
+
+/*!
+    \brief      configure the RCU of peripherals
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void rcu_config(void)
+{
+    /* enable the clock of peripherals */
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_DAC);
+    rcu_periph_clock_enable(RCU_TIMER5);
+    rcu_periph_clock_enable(RCU_TIMER6);
+}
+
+/*!
+    \brief      configure the related GPIO
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void gpio_config(void)
+{
+    /* once enabled the DAC, the corresponding GPIO pin is connected to the DAC converter automatically */
+    gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, GPIO_PIN_4 | GPIO_PIN_5);
+}
+
+/*!
+    \brief      configure the DAC
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void dac_config(void)
+{
+    dac_deinit();
+    /* configure the DAC0 */
+    dac_trigger_source_config(DAC0, DAC_TRIGGER_T5_TRGO);
+    dac_trigger_enable(DAC0);
+    dac_wave_mode_config(DAC0, DAC_WAVE_MODE_LFSR);
+    dac_lfsr_noise_config(DAC0, DAC_LFSR_BITS9_0);
+    dac_output_buffer_disable(DAC0);
+
+    /* configure the DAC1 */
+    dac_trigger_source_config(DAC1, DAC_TRIGGER_T6_TRGO);
+    dac_trigger_enable(DAC1);
+    dac_wave_mode_config(DAC1, DAC_WAVE_MODE_LFSR);
+    dac_lfsr_noise_config(DAC1, DAC_LFSR_BITS10_0);
+    dac_output_buffer_disable(DAC1);
+
+    /* enable DAC concurrent mode and set data */
+    dac_concurrent_enable();
+    dac_concurrent_data_set(DAC_ALIGN_12B_R, 0x7F0, 0x7F0);
+}
+
+/*!
+    \brief      configure the TIMER5
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void timer5_config(void)
+{
+    /* configure the TIMER5 */
+    timer_prescaler_config(TIMER5, 0xF, TIMER_PSC_RELOAD_UPDATE);
+    timer_autoreload_value_config(TIMER5, 0x1FF);
+    timer_master_output_trigger_source_select(TIMER5, TIMER_TRI_OUT_SRC_UPDATE);
+
+    timer_enable(TIMER5);
+}
+
+/*!
+    \brief      configure the TIMER6
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void timer6_config(void)
+{
+    /* configure the TIMER6 */
+    timer_prescaler_config(TIMER6, 0xF, TIMER_PSC_RELOAD_UPDATE);
+    timer_autoreload_value_config(TIMER6, 0x1FF);
+    timer_master_output_trigger_source_select(TIMER6, TIMER_TRI_OUT_SRC_UPDATE);
+
+    timer_enable(TIMER6);
+}
